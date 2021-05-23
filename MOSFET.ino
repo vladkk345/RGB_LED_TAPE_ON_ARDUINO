972158532fd5828a50ac5e7ce83aa982605d2ae0
vladkk345 <55635774+vladkk345@users.noreply.github.com>
2021-05-23 19:06:30 +0300

    Update MOSFET.ino

--- MOSFET.ino
+++ MOSFET.ino
@@ -1,18 +1,18 @@
-/*Светодиодная лента на MOSFET транзисторах с возможностью регулировки
-яркости посредством ШИМ, используя потенциометр
+/*LED tape on MOSFET transistors with possible control
+brightness using PWM and using potentiometer
 */
-#define SIGNAL_R 6  // выходной сигнал с MOSFET для красного
-#define SIGNAL_G 9  // выходной сигнал с MOSFET для зеленого
-#define SIGNAL_B 10 // выходной сигнал с MOSFET для синего
-#define AN_SIGNAL_ONE   5 // аналоговый сигнал
-#define AN_SIGNAL_TWO   6 // аналоговый сигнал
-#define AN_SIGNAL_THREE 7 // аналоговый сигнал
+#define SIGNAL_R 6  // output signal from MOSFET for red
+#define SIGNAL_G 9  // output signal from MOSFET for green
+#define SIGNAL_B 10 // output signal from MOSFET for blue
+#define AN_SIGNAL_ONE   5 // analog signal
+#define AN_SIGNAL_TWO   6 // analog signal
+#define AN_SIGNAL_THREE 7 // analog signal
 
-int PWM_R, PWM_G, PWM_B; // объявление переменных для 3 основных цветов
+int PWM_R, PWM_G, PWM_B; // declaration variables for 3 basic color
 
 void setup() 
 { 
-  // Настройка 3 сигналов как выходы
+  // Settings 3 signal as output
   pinMode(SIGNAL_R, OUTPUT);
   pinMode(SIGNAL_G, OUTPUT);
   pinMode(SIGNAL_B, OUTPUT); 
@@ -20,20 +20,20 @@ void setup()
 
 void loop() 
 {
-  // Получение данных для первого транзистора
-  PWM_R = analogRead(AN_SIGNAL_ONE); // чтение значения с сигнала А5(аналоговый) и присваивание переменной PWM_R
-  PWM_R = map(PWM_R, 0, 1023, 0, 255); // преобразование данных из цифрового формата в аналоговый
-  PWM_R = constrain(PWM_R, 0, 255); // ограничение диапазона
-  analogWrite(SIGNAL_R, PWM_R); // запись данных в значение SIGNAL_R
+  // Getting data for first transistor
+  PWM_R = analogRead(AN_SIGNAL_ONE); // reading value from signal А5(analog) и assingment variable PWM_R
+  PWM_R = map(PWM_R, 0, 1023, 0, 255); // converting data from digital format to analog
+  PWM_R = constrain(PWM_R, 0, 255); // constrain range
+  analogWrite(SIGNAL_R, PWM_R); // setting data in value SIGNAL_R
   
-  // Получение данных для второго транзистора (аналогично)
-  PWM_G = analogRead(AN_SIGNAL_TWO); // чтение с сигнала А6
+  // Getting data for second transistor (analogically)
+  PWM_G = analogRead(AN_SIGNAL_TWO); // reading from signal А6
   PWM_G = map(PWM_G, 0, 1023, 0, 255);
   PWM_G = constrain(PWM_G, 0, 255);
   analogWrite(SIGNAL_G, PWM_G);
   
-  // Получение данных для третьего транзистора (аналогично)
-  PWM_B = analogRead(AN_SIGNAL_THREE);// чтение с сигнала А7
+  // Getting data for third transistor (analogically)
+  PWM_B = analogRead(AN_SIGNAL_THREE);// reading from signal А7
   PWM_B = map(PWM_B, 0, 1023, 0, 255);
   PWM_B = constrain(PWM_B, 0, 255);
   analogWrite(SIGNAL_B, PWM_B);
